0417eaa1dffddf9ae4ed0882cf80964a818bc354
Ryan Brooks <ryanbrooksis@gmail.com>
2015-04-11 12:15:44 +0100

    more stuffz

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
